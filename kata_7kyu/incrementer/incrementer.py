911eacdbd52de6c6d47a627c1815f532fa86c132
Patrycja <patrycja.szwaba@gmail.com>
2023-08-12 19:29:44 +0200

    add def incrementer

---
+++ kata_7kyu/incrementer/incrementer.py
@@ -0,0 +1,11 @@
+def incrementer(nums):
+    couter = 1
+    new_nums = []
+    for i in nums:
+        number = i + couter
+        couter += 1
+        if number > 9:
+            new_nums.append(int(str(number)[-1]))
+        else:
+            new_nums.append(number)
+    return new_nums
