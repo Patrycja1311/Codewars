94699665c1d7d646fc8bdc448ab31dd18ec8154b
Patrycja <patrycja.szwaba@gmail.com>
2024-01-11 17:36:34 +0100

    add def weather_info, update README

---
+++ kata_8kyu/grasshopper_debug/grasshopper_debug.py
@@ -0,0 +1,11 @@
+def convert_to_celsius(temperature):
+    celsius = (temperature - 32) * (5/9)
+    return celsius
+
+
+def weather_info(temp):
+    c = convert_to_celsius(temp)
+    if c < 0:
+        return f'{c} is freezing temperature'
+    else:
+        return f'{c} is above freezing temperature'
