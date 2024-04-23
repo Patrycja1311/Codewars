e29dea76ce65f705b409d2ef70229caf177e9a72
Patrycja <patrycja.szwaba@gmail.com>
2024-04-23 19:53:34 +0200

    add def am_I_afraid, update README

---
+++ kata_7kyu/selective_fear_of_numbers/selective_fear_of_numbers.py
@@ -0,0 +1,16 @@
+def am_I_afraid(day, num):
+    if day == "Monday":
+        return num == 12
+    elif day == "Tuesday":
+        return num > 95
+    elif day == "Wednesday":
+        return num == 34
+    elif day == "Thursday":
+        return num == 0
+    elif day == "Friday":
+        return num % 2 == 0
+    elif day == "Saturday":
+        return num == 56
+    elif day == "Sunday":
+        return abs(num) == 666
+    return False
