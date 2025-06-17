45386b1e5a220cfd624ea8397e15105aed11e26e
Patrycja <patrycja.szwaba@gmail.com>
2025-06-17 21:02:00 +0200

    add def baby_shark_lyrics, update README

---
+++ kata_7kyu/baby_shark_lyrics_generator/baby_shark_lyrics_generator.py
@@ -0,0 +1,2 @@
+def baby_shark_lyrics():
+    return'\n'.join((s+', '+'doo '*5+'doo\n')*3+s+'!'for s in["Baby shark","Mommy shark","Daddy shark","Grandma shark","Grandpa shark","Let's go hunt"])+'\nRun away,…'
