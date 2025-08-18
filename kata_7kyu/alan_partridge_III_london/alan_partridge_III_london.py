862dfcfcd6ce95bca1d1d178b891620ac1ad3316
Patrycja <patrycja.szwaba@gmail.com>
2025-08-18 23:19:31 +0200

    add def alan, update README

---
+++ kata_7kyu/alan_partridge_III_london/alan_partridge_III_london.py
@@ -0,0 +1,3 @@
+def alan(arr):
+    required = ["Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway"]
+    return "Smell my cheese you mother!" if all(stop in arr for stop in required) else "No, seriously, run. You will miss it."
