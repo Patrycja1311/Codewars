1b3ee769834a384d7da33ea541dc27879b8d50a1
Patrycja <patrycja.szwaba@gmail.com>
2026-08-25 20:42:39 +0200

    add def each_char, update README

---
+++ kata_7kyu/generating_markdowns/generating_markdowns.py
@@ -0,0 +1,7 @@
+def generate_markdowns(markdown, text, url_or_language):
+    if markdown == "link":
+        return f"[{text}]({url_or_language})"
+    if markdown == "img":
+        return f"![{text}]({url_or_language})"
+    if markdown == "code":
+        return f"```{url_or_language}\n{text}\n```"
